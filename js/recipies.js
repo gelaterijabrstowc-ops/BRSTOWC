567a166ad9f873492b9fbcaecf00e2defbf3b814
BRSTOWC <gelaterijabrstowc@gmail.com>
2026-08-04 22:15:33 +0200

    Update recipies.js

--- js/recipies.js
+++ js/recipies.js
@@ -74,3 +74,75 @@ function addRecipe(){
 
 
 }
+
+function displayRecipes(){
+
+    const recipeGrid = document.getElementById("recipeGrid");
+
+    let recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+
+
+    recipes.forEach(recipe => {
+
+
+        const card = document.createElement("article");
+
+        card.className = "recipe-card";
+
+
+        card.innerHTML = `
+
+            <div class="recipe-info">
+
+                <span class="category">
+                    ${recipe.category}
+                </span>
+
+                <h2>
+                    ${recipe.name}
+                </h2>
+
+
+                <p>
+                    ${recipe.ingredients}
+                </p>
+
+
+                <button onclick="viewRecipe(${recipe.id})">
+                    Ogled recepta
+                </button>
+
+            </div>
+
+        `;
+
+
+        recipeGrid.appendChild(card);
+
+
+    });
+
+}
+
+
+
+function viewRecipe(id){
+
+    let recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+
+    const recipe = recipes.find(r => r.id === id);
+
+
+    alert(
+        recipe.name +
+        "\n\nSestavine:\n" +
+        recipe.ingredients +
+        "\n\nPostopek:\n" +
+        recipe.steps
+    );
+
+}
+
+
+
+displayRecipes();
