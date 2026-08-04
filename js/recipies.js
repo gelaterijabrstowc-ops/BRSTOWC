0ff7a9eeb22303236a791998a5728aed0fdfba21
BRSTOWC <gelaterijabrstowc@gmail.com>
2026-08-04 22:07:53 +0200

    Update recipies.js

--- js/recipies.js
+++ js/recipies.js
@@ -1,1 +1,12 @@
+function openRecipeForm(){
 
+    document.getElementById("recipeModal").style.display="flex";
+
+}
+
+
+function closeRecipeForm(){
+
+    document.getElementById("recipeModal").style.display="none";
+
+}
